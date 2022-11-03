874dafddd3e9568a905ab386ec24dce5739f07d8
hajubal <hajubal@gmail.com>
2022-11-03 20:55:39 +0900

    memory userdetail service sample

--- 2-userservice/src/main/java/me/synology/hajubal/springsecurity/security/configs/SecurityConfig.java
+++ 2-userservice/src/main/java/me/synology/hajubal/springsecurity/security/configs/SecurityConfig.java
@@ -1,16 +1,25 @@
 package me.synology.hajubal.springsecurity.security.configs;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@RequiredArgsConstructor
 @Slf4j
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -30,4 +39,17 @@ public class SecurityConfig {
                 .and()
                 .build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                .username("user")
+                .password(bCryptPasswordEncoder.encode("user"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+
 }
